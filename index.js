ab268f43b72cf1aa090c999d144475bf2885e142
amitozs999 <ekas0001@gmail.com>
2019-08-23 15:16:08 +0530

    commit

--- index.js
+++ index.js
@@ -24,8 +24,42 @@ app.use('/',require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.listen(8020,function(req,res){
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        
+        },
+        function(err){
+            console.log(err ||  'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
+
+
+app.listen(8010,function(req,res){
 
 
 
